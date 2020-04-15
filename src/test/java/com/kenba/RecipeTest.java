4489cca4dbac061ebc1da62beee788444b300eb1
黃鼎鈞 <derek19960312@webcomm.com.tw>
2020-04-15 15:04:42 +0800

    add Recipe test and update projectName

--- src/test/java/com/kenba/RecipeTest.java
+++ src/test/java/com/kenba/RecipeTest.java
@@ -1,5 +1,11 @@
 package com.kenba;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,14 +19,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.kenba.main.KenbaApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = KenbaApplication.class)
 @AutoConfigureMockMvc
 public class RecipeTest {
 
@@ -32,17 +34,37 @@ public class RecipeTest {
 	@Before
 	public void init() {
 		httpHeaders = new HttpHeaders();
-		httpHeaders.add("Content-Type", "application/json");
+		httpHeaders.add("Content-Type", "application/json;charset=utf-8");
 	}
 
 	@Test
 	public void testCreateRecipe() throws Exception {
 
 		JSONObject request = new JSONObject();
-		request.put("name", "Harry Potter");
-		request.put("price", 450);
+		request.put("name", "Apple Pie");
+		request.put("count", 123);
+		
+		JSONArray recipeIngredients = new JSONArray();
+		
+		JSONObject recipeIngredient1 = new JSONObject();
+		recipeIngredient1.put("riCount", 200);
+		recipeIngredient1.put("riPrice", 16);
+		JSONObject ingredient = new JSONObject();
+		ingredient.put("id", 1);
+		recipeIngredient1.put("ingredient",ingredient);
+		recipeIngredients.put(recipeIngredient1);
+		
+		JSONObject recipeIngredient2 = new JSONObject();
+		recipeIngredient2.put("riCount", 100);
+		recipeIngredient2.put("riPrice", 1600);
+		JSONObject ingredient2 = new JSONObject();
+		ingredient2.put("id", 2);
+		recipeIngredient2.put("ingredient",ingredient2);
+		recipeIngredients.put(recipeIngredient2);
+		
+		request.put("reciepIngredients", recipeIngredients);
 
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/products").headers(httpHeaders)
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/recipe").headers(httpHeaders)
 				.content(request.toString());
 
 		mockMvc.perform(requestBuilder)
@@ -50,8 +72,28 @@ public class RecipeTest {
 				.andExpect(status().isCreated())
 				.andExpect(jsonPath("$.id").hasJsonPath())
 				.andExpect(jsonPath("$.name").value(request.getString("name")))
-				.andExpect(jsonPath("$.price").value(request.getInt("price"))).andExpect(header().exists("Location"))
-				.andExpect(header().string("Content-Type", "application/json;charset=UTF-8"));
+				.andExpect(jsonPath("$.count").value(request.getInt("count")))
+				.andExpect(header().exists("Location"))
+				.andExpect(header().string("Content-Type", "application/json"));
+	}
+	
+	@Test
+	public void testGetRecipe() throws Exception {
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/recipe/1").headers(httpHeaders);
+
+		mockMvc.perform(requestBuilder)
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").hasJsonPath())
+				.andExpect(jsonPath("$.count").value(30))
+				.andExpect(jsonPath("$.name").value("蘋果派"));
+				
+		RequestBuilder requestBuilder2 = MockMvcRequestBuilders.get("/recipe/10").headers(httpHeaders);
+		
+		mockMvc.perform(requestBuilder2)
+				.andDo(print())
+				.andExpect(status().isNotFound());
 	}
 
 }
