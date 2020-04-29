b5e3529734eb2391cba0a0c072948714af6b4ce4
黃鼎鈞 <derek19960312@webcomm.com.tw>
2020-04-29 17:55:19 +0800

    k8s build success

--- sql/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'derek'@'localhost' IDENTIFIED BY 'motorolamb525';
-GRANT ALL PRIVILEGES ON kenba.* TO 'derek'@'localhost';
-
-CREATE USER 'derek'@'172.19.0.3' IDENTIFIED BY 'motorolamb525';
-GRANT ALL PRIVILEGES ON kenba.* TO 'derek'@'172.19.0.1';
-
-CREATE USER 'derek'@'172.19.0.3' IDENTIFIED BY 'motorolamb525';
-GRANT ALL PRIVILEGES ON kenba.* TO 'derek'@'172.19.0.3';
